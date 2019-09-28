17a9bc980bcb18d7b8d1149e2f2b2d20e62fc3b6
Esteve Segura <estevesegura.gir@gmail.com>
2019-09-29 00:40:53 +0200

    upload picture feature

--- src/bot.js
+++ src/bot.js
@@ -2,17 +2,18 @@ require('tools-for-instagram');
 const utils = require('./utils.js');
 const config = require('./config.json');
 
-
-//Start with short timeout with async
-setTimeout(async() => {
+(async () => {
     let ig = await login();
 
-    //This interval happends 1 time per minut
-    //To check intervals
+    //This interval happends 1 time per minut to check intervals
     setInterval(async() => {
         let currentDate = new Date()
+
+        console.log(currentDate.getHours())
+        console.log(currentDate.getMinutes())
         let getHour = currentDate.getHours();
         let getMinutes = currentDate.getMinutes();
+        
 
         //Check intervals
         config.intervals.forEach(async (time) => {
@@ -22,7 +23,7 @@ setTimeout(async() => {
                 let allPostsToGiveLike = await utils.getPostsFromHashtags(ig,config.hashtags)
                 for(let i = 0 ; i < config.likesPerInterval ; i++){
                     await sleep(utils.randomIntInc(3,220))
-                    likeMediaId(ig,allPostsToGiveLike[utils.randomIntInc(0,allPosts.length)].pk)
+                    likeMediaId(ig,allPostsToGiveLike[utils.randomIntInc(0,allPostsToGiveLike.length)].pk)
                 }
             }
             
@@ -31,9 +32,17 @@ setTimeout(async() => {
                 let allPostsToExtractUser = await utils.getPostsFromHashtags(ig,config.hashtags)
                 for(let i = 0 ; i < config.followsPerInterval ; i++){
                     await sleep(utils.randomIntInc(3,220))
-                    followUser(ig, allPostsToExtractUser[utils.randomIntInc(0,allPosts.length)].user.pk)
+                    followUser(ig, allPostsToExtractUser[utils.randomIntInc(0,allPostsToExtractUser.length)].user.pk)
                 }
             }
+
+            //Check if is time to uppload picture
+            if(getHour == time.startHour && getMinutes == time.startMinute && time.action == "uploadPicture"){
+                let fileToUpload = await utils.getFileToUpload('./img')
+                await uploadPicture(ig, fileToUpload.caption, fileToUpload.pathOfFileToUpload)
+                await utils.backupFolder('./upload')
+            }
+
         });
     }, 60000);
-}, 400);
+})();
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+const fs = require('fs')
+const config = require('./config.json');
+
+
+
+//get posts from array of hashtags
 async function getPostsFromHashtags(ig,arrayOfHashtags){
     let allPosts = []
     for(let i = 0 ; i < arrayOfHashtags.length ; i++){
@@ -7,9 +13,58 @@ async function getPostsFromHashtags(ig,arrayOfHashtags){
     return [].concat.apply([], allPosts)
 }
 
-
+//get random number
 function randomIntInc(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-module.exports = {randomIntInc , getPostsFromHashtags}
+//check files in folder, get 1 file, move to "./upload"
+function getFileToUpload(path){
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, function(err, items) {
+            if(items.length != 0){
+                let pathOfFileToUpload = `./img/${items[0]}`;
+                fs.rename(`./img/${items[0]}`, `./upload/${items[0]}`, (err) => {
+                    if(err){
+                        reject(err)
+                    }else{
+                        caption = config.files.filter((fileItem) => {
+                            if(fileItem.filePath == pathOfFileToUpload)
+                                return fileItem.caption
+                        })
+                        resolve({
+                            pathOfFileToUpload:pathOfFileToUpload,
+                            caption: caption
+                        })
+                    }
+                });
+            }else{
+                reject("Not files found")
+            }
+        });
+    });
+}
+
+function backupFolder(path){
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, function(err, items) {
+            if(items.length != 0){
+                fs.rename(`./upload/${items[0]}`, `./backup/${items[0]}`, (err) => {
+                    if(err){
+                        reject(err)
+                    }else{
+                        resolve("file moved to backup")
+                    }
+                });
+            }else{
+                reject("Not files found")
+            }
+        });
+    });
+}
+
+
+
+
+
+module.exports = {randomIntInc , getPostsFromHashtags, getFileToUpload, backupFolder}
